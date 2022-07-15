9ed39e04b82f7823ddb4f89fd409c7c624fefb3f
Dmitriy <shadowgods@mail.ru>
2022-07-16 03:23:28 +0500

    Задание 2 Версия ОС и год устр-ва

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,21 @@
 public class Main {
     public static void main(String[] args) {
-        int clientOs=0;
-        if (clientOs ==0)
+        int clientOs=1;
+        int clientDeviceYear=2000;
+        if (clientOs ==0 && clientDeviceYear>=2015 )
             System.out.println("Установите версию приложения для iOS по ссылке");
-        else System.out.println("Установите версию приложения для Android по ссылке");
+            else {
+            if (clientOs ==0 && clientDeviceYear < 2015)
+                System.out.println("Установите облегченную версию приложения для iOS по ссылке");
+        }
+
+        if (clientOs !=0 && clientDeviceYear>=2015 )
+            System.out.println("Установите версию приложения для Android по ссылке");
+            else {
+            if (clientOs != 0 && clientDeviceYear < 2015)
+                System.out.println("Установите облегченную версию приложения для Android по ссылке");
+        }
+
+
     }
 }
