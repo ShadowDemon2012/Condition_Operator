bbe4adc70a038f2d7f37b93ea0db2f6d7f7ae8d2
Dmitriy <shadowgods@mail.ru>
2022-07-16 22:06:39 +0500

    Задание 4 Доставка(исправленное дубль 2)

--- src/Main.java
+++ src/Main.java
@@ -1,16 +1,13 @@
 public class Main {
     public static void main(String[] args) {
-        int deliveryDistance = 80;
-        int deliveryTime = 0;
-        if (deliveryDistance <=20) { deliveryTime =deliveryTime + 1;
-            System.out.println("Потребуется дней "+ deliveryTime);
+        int deliveryDistance = 95;
+        int deliveryTime = 1;
+
+        if (deliveryDistance>20) { deliveryTime++;}
+
+        if (deliveryDistance >60) {deliveryTime++;
         }
-        if (deliveryDistance >20 && deliveryDistance <=60) {deliveryTime = deliveryTime +2;
-            System.out.println("Потребуется дней "+ deliveryTime);
-                   }
-        if (deliveryDistance >60 && deliveryDistance <=100) {deliveryTime = deliveryTime +3;
-            System.out.println("Потребуется дней "+ deliveryTime);
-        }
+        System.out.println("Потребуется дней "+ deliveryTime);
 
 
     }
