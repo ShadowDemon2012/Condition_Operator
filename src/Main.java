454dcb3f9471724a382d8145ae968f779114b012
Dmitriy <shadowgods@mail.ru>
2022-07-16 09:18:34 +0500

    Задание 4 Доставка

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,17 @@
 public class Main {
     public static void main(String[] args) {
-        int year=2008;
-        boolean leapYear = (year % 4 ==0 && year % 100!=0 || year %400==0);
-        if ( leapYear ) System.out.println("год високосный");
-        else System.out.println("год не високосный");
+        int deliveryDistance = 95;
+        int deliveryTime = 0;
+        if (deliveryDistance <=20) { deliveryTime =deliveryTime + 1;
+            System.out.println("Потребуется дней "+ deliveryTime);
+        }
+        if (deliveryDistance >20 && deliveryDistance <=60) {deliveryTime = deliveryTime +2;
+            System.out.println("Потребуется дней "+ deliveryTime);
+                   }
+        if (deliveryDistance >60 && deliveryDistance <=100) {deliveryTime = deliveryTime +3;
+            System.out.println("Потребуется дней "+ deliveryTime);
+        }else System.out.println("В такую даль не доставляем ((");
+
+
     }
 }
