27f1916dee37b1e4d33ec7e68939a279b34a8bd5
Dmitriy <shadowgods@mail.ru>
2022-07-16 20:15:12 +0500

    Задание 6 Выдача кредитки

--- src/Main.java
+++ src/Main.java
@@ -1,20 +1,18 @@
+//Людям старше (или равно) 23 лет мы предоставляем лимит в размере 3 зарплат.
+//Людям младше 23 лет мы предоставляем лимит в размере 2 зарплат.
+//Если заработная плата клиента выше (или равно) 50 тысяч, мы увеличиваем лимит в 1.2 раза.
+//Если заработная плата выше (или равно) 80 тысяч, мы увеличиваем лимит в 1.5 раза.
+//Наша задача: написать программу, которая показывает, какую сумму на кредитке может получить клиент.
 public class Main {
-    public static void main(String[] args) {
-        int clientOs=1;
-        int clientDeviceYear=2000;
-        if (clientOs ==0 && clientDeviceYear>=2015 )
-            System.out.println("Установите версию приложения для iOS по ссылке");
-            else {
-            if (clientOs ==0 && clientDeviceYear < 2015)
-                System.out.println("Установите облегченную версию приложения для iOS по ссылке");
-        }
-
-        if (clientOs !=0 && clientDeviceYear>=2015 )
-            System.out.println("Установите версию приложения для Android по ссылке");
-            else {
-            if (clientOs != 0 && clientDeviceYear < 2015)
-                System.out.println("Установите облегченную версию приложения для Android по ссылке");
-        }
+        public static void main(String[] args) {
+        int age=19;
+        float salary=58_000F;
+        float creditLimit= salary;
+        if (age >=23) creditLimit=salary * 3;
+           else if (age>=18 && age<23) creditLimit= salary*2;
+        if (salary >=50_000 && salary <80_000) creditLimit = creditLimit*1.2F;
+           else if (salary>=80_000) creditLimit= creditLimit*1.5F;
+            System.out.println("Мы готовы выдать вам карту с лимитом " + (int) creditLimit+ " рублей");
 
 
     }
